e86fdb6b9aaf704cfaa07a693e2ac9673eaac5c3
goncalocoval <covalgoncalo2003@gmail.com>
2025-06-19 16:29:48 +0100

    env variables implemented, stable vercel version

--- client/src/app/axiosInstance.ts
+++ client/src/app/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
